e7290dde3c40e9b7b5efee36a9be1b88a3cc1fb5
mihirkate <mihirkate13@gmail.com>
2024-07-30 02:22:35 +0530

    added

--- init/index.js
+++ init/index.js
@@ -27,7 +27,7 @@ const initDB = async () => {
   await Listing.deleteMany({});
   initData.data = initData.data.map((obj) => ({
     ...obj,
-    owner: "66061fbdccfd26a7db92156e",
+    owner: "66a7c445bc7a2dc9a7994c55",
   }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
---
+++ js/map.js
@@ -0,0 +1,28 @@
+try {
+    mapboxgl.accessToken = mapToken;
+
+    if (listing && listing.geometry && listing.geometry.coordinates) {
+        const map = new mapboxgl.Map({
+            container: "map", // container ID
+            style: "mapbox://styles/mapbox/streets-v12",
+            center: listing.geometry.coordinates, // starting position [lng, lat]
+            zoom: 15, // starting zoom
+        });
+
+        console.log("This are ", listing.geometry.coordinates);
+
+        const marker = new mapboxgl.Marker({ color: "red" })
+            .setLngLat(listing.geometry.coordinates)
+            .setPopup(
+                new mapboxgl.Popup({ offset: 25 }).setHTML(
+                    `<h4>${listing.location}</h4>`
+                )
+            )
+            .addTo(map);
+    } else {
+        throw new Error("Please fill the details carefully!");
+    }
+} catch (err) {
+    console.error("An error occurred:", err);
+    throw new Error("Please fill the details carefully!");
+}
